6cb11056b07518563855abeabb2b20d2f45bfa62
Gabriel Turgeon <gabriel.turgeon@flo.com>
2024-09-22 10:04:22 -0400

    make navbar responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,8 +8,8 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="bg-blue-800 text-white py-4 font-brockmann">
-            <div className="mx-auto px-4 flex justify-between items-center">
+        <nav className="bg-blue-800 text-white py-4 relative font-brockmann">
+            <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
                 <div>
                     <h1 className=" text-2xl font-bold">FromScript</h1>
                 </div>
@@ -24,9 +24,9 @@ const Navbar = () => {
                     className={`${isMenuOpen ? 'flex' : 'hidden'} absolute md:relative top-full left-0 right-0 z-10 flex-col md:flex-row md:flex bg-blue-800 md:bg-transparent shadow-md md:shadow-none`}
                     style={{zIndex: 1000}}  // Ensure it's on top of other content
                 >
-                    <a href="#" className="px-4 py-2 block text-sm hover:underline">Services</a>
-                    <a href="#" className="px-4 py-2 block text-sm hover:underline">About</a>
-                    <a href="#" className="px-4 py-2 block text-sm hover:underline">Contact</a>
+                    <a href="#services" className="px-4 py-2 block text-sm text-center hover:underline">Services</a>
+                    <a href="#about" className="px-4 py-2 block text-sm text-center hover:underline">About</a>
+                    <a href="#contact" className="px-4 py-2 block text-sm text-center hover:underline">Contact</a>
                 </div>
             </div>
         </nav>
