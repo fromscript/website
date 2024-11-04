c7637582373b0c39b186909983ec08ae1a279e97
nikopole <gabriel.turgeon@fromscript.ca>
2024-11-03 21:39:21 -0500

    about responsive

--- sections/AboutSection.tsx
+++ sections/AboutSection.tsx
@@ -1,55 +1,59 @@
 "use client";
 
-import React from 'react';
-import {Parallax, ParallaxProvider} from 'react-scroll-parallax';
+import React from "react";
+import {Parallax, ParallaxProvider} from "react-scroll-parallax";
+import useIsMobile from "../hook/useIsMobile";
 
 const AboutSection: React.FC = () => {
-    return (
-        <ParallaxProvider>
-            <section id="about" className="w-full py-12 h-auto bg-white flex flex-col items-center relative overflow-hidden">
-                <div className="self-stretch flex justify-center items-center flex-col overflow-hidden h-72">
-                    <Parallax
-                        className="text-blue-700 text-8xl font-bold font-brockmann"
-                        translateX={[60, -40]}
-                        easing="easeOutQuad"
-                        disabled={false}
-                    >
-                        La technologie
-                    </Parallax>
-                    <Parallax
-                        className="text-start text-blue-700 text-8xl font-bold font-brockmann"
-                        translateX={[-60, 40]}
-                        easing="easeOutQuad"
-                        disabled={false}
-                    >
-                        accessible à tous
-                    </Parallax>
-                </div>
+  // Check if the screen size is mobile using `window.matchMedia`
+  const isMobile = useIsMobile();
 
-                <div className="self-stretch h-auto bg-white flex justify-center items-center gap-20">
-                    <div className="flex flex-col justify-center items-center text-left">
-                        <div className="text-blue-500 text-[40px] font-bold font-brockmann leading-tight">
-                            Découvrez l'expertise de<br/>FromScript en développement<br/>logiciel et sécurité<br/>informatique.
-                        </div>
-                    </div>
-                    <div className="flex flex-col justify-center items-center text-left">
-                        <div className="justify-start">
-                            <p className="text-blue-700 text-lg font-normal leading-7">
-                                Avec plus de 8 ans d'expérience, FromScript se spécialise dans le<br/>développement de
-                                solutions logicielles sur mesure et la sécurité<br/>informatique. Nous nous engageons à
-                                rendre la technologie<br/>accessible tout en garantissant une sécurité optimale.
-                            </p>
-                            <br/>
-                            <p className="text-blue-700 text-lg font-bold leading-7">
-                                Nos valeurs fondamentales sont l'accompagnement<br/>personnalisé, la qualité et la
-                                satisfaction client.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </ParallaxProvider>
-    );
+  return (
+    <ParallaxProvider>
+      <section id="about"
+               className="w-full pb-12 md:py-12 bg-white flex flex-col items-center relative overflow-hidden">
+        {/* Parallax Effect Section */}
+        <div className="self-stretch flex justify-center items-center flex-col overflow-hidden h-72">
+          <Parallax className="text-blue-700 text-5xl md:text-8xl font-bold font-brockmann"
+                    translateX={[60, -40]}
+                    easing="easeOutQuad"
+                    disabled={isMobile} // Disable parallax on mobile
+          >
+            La technologie
+          </Parallax>
+          <Parallax className="text-center md:text-start text-blue-700 text-5xl md:text-8xl font-bold font-brockmann"
+                    translateX={[-60, 40]}
+                    easing="easeOutQuad"
+                    disabled={isMobile} // Disable parallax on mobile
+          >
+            accessible à tous
+          </Parallax>
+        </div>
+
+        {/* Text Content Section */}
+        <div className="flex =self-stretch justify-center items-center">
+          <div className="md:max-w-5xl h-auto bg-white flex flex-col md:flex-row justify-center items-center gap-10 md:gap-20 px-4 md:px-0">
+            <div className="flex flex-col justify-center items-center">
+              <div className="text-blue-500 text-center md:text-start text-2xl md:text-[40px] text-wrap font-bold font-brockmann leading-tight">
+                Découvrez l'expertise de FromScript en développement logiciel et sécurité informatique.
+              </div>
+            </div>
+            <div className="flex flex-col justify-center items-center text-left">
+              <div className="justify-start text-center md:text-start">
+                <p className="text-blue-700 text-base md:text-lg font-normal leading-6 md:leading-7">
+                  Avec plus de 8 ans d'expérience, FromScript se spécialise dans le développement de solutions
+                  logicielles sur mesure et la sécurité informatique. Nous nous engageons à rendre la technologie
+                  accessible tout en garantissant une sécurité optimale. </p><br/>
+                <p className="text-blue-700 text-base md:text-lg font-bold leading-6 md:leading-7">
+                  Nos valeurs fondamentales sont l'accompagnement personnalisé, la qualité et la satisfaction
+                  client. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </ParallaxProvider>
+  );
 };
 
 export default AboutSection;
